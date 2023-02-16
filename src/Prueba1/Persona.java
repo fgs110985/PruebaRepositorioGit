b94a2c2e0c49e88969e5a50afaf1f53eedb9d636
Paco <fgarcia96@alu.ucam.edu>
2023-02-16 13:24:50 +0100

    Modificar Persona.java y creado Colegio.java

--- src/Prueba1/Persona.java
+++ src/Prueba1/Persona.java
@@ -2,4 +2,23 @@ package Prueba1;
 
 public class Persona {
 
+	private String nombre;
+
+	public Persona(String nombre) {
+
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String toString() {
+		return "Persona [nombre=" + nombre + "]";
+	}
+
 }
